cb5099616e4215a534d89dc2e8d01efac4ef200d
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-08-17 21:45:34 +0530

    Encode and Decode String added

---
+++ Array and Hashing/Encode and Decode Strings/Answer6.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Answer6 {
+    public static String encode(List<String> str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.size(); i++) {
+            String key = str.get(i);
+            sb.append(key.length()).append("#").append(key);
+        }
+        return sb.toString();
+
+    }
+
+    public static List<String> decode(String str) {
+        ArrayList<String> result = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+            int length = Integer.parseInt(str.substring(i, j));
+            i = j + 1;
+            result.add(str.substring(i, i + length));
+            i = i + length;
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter the Elements in the Array");
+        ArrayList<String> str = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            str.add(sc.nextLine());
+        }
+        String encoded = encode(str);
+        System.out.println("Encoded: " + encoded);
+
+        List<String> decoded = decode(encoded);
+        System.out.println("Decoded: " + decoded);
+        sc.close();
+
+    }
+}
