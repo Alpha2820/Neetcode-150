3cbf0e97bfc800970dbdabb863660dd91810a403
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-08-20 22:29:14 +0530

    Time and Space Compexity have been added

--- Array and Hashing/Longest Consecutive Sequence/Answer9.java
+++ Array and Hashing/Longest Consecutive Sequence/Answer9.java
@@ -1,46 +1,43 @@
 import java.util.*;
 
-public class Answer9{
+public class Answer9 {
 
-    public static int logenstCommonSubsequence(int arr[])
-    {
-        HashSet<Integer>set = new HashSet<>();
-        for(int i = 0 ; i<nums.length ; i++)
-        {
-            set.add(nums[i]);
+    public static int logenstCommonSubsequence(int arr[]) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
         }
         int maxStreak = 0;
-        for(int j = 0 ; j<nums.length ; j++)
-        {
-            if(set.contains(nums[j]-1))
-            {
+        for (int j = 0; j < arr.length; j++) {
+            if (set.contains(arr[j] - 1)) {
                 int currNum = arr[j];
                 int currStreak = 1;
-                while(set.contains(currNum+1))
-                {
+                while (set.contains(currNum + 1)) {
                     currNum++;
                     currStreak++;
                 }
-                maxStreak = Math.max(maxStreak,currStreak);
+                maxStreak = Math.max(maxStreak, currStreak);
             }
         }
         return maxStreak;
     }
-    public static void main(String args[])
-    {
+
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
         int n = sc.nextInt();
         int[] arr = new int[n];
         System.out.println("Enter the elements of the array");
-        for(int i=0;i<n;i++)
-        {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         int ans = logenstCommonSubsequence(arr);
-        System.out.println("The longest consecutive sequence is: "+ans);
+        System.out.println("The longest consecutive sequence is: " + ans);
         sc.close();
     }
 }
-// Time Complexity: O(n) - We traverse the array twice, but each operation inside the loops is O(1) due to the HashSet operations.
-// Space Complexity: O(n) - We use a HashSet to store the elements of the array, which takes up additional space proportional to the number of unique elements in the array.
+// Time Complexity: O(n) - We traverse the array twice, but each operation
+// inside the loops is O(1) due to the HashSet operations.
+// Space Complexity: O(n) - We use a HashSet to store the elements of the array,
+// which takes up additional space proportional to the number of unique elements
+// in the array.
