0d5b9a658c51ab9affc9d71a23b81e00b577efd3
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-08-26 19:14:24 +0530

    Valid Parentheses have been added

---
+++ Stack/Valid Parentheses/Answer15.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Answer15 {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (st.isEmpty() && (ch == ')' || ch == ']' || ch == '}')) {
+                return false;
+            }
+            if (ch == ')' && st.peek() == '(') {
+                st.pop();
+            } else if (ch == '}' && st.peek() == '{') {
+                st.pop();
+            } else if (ch == ']' && st.peek() == '[') {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string of parentheses: ");
+        String str = sc.nextLine();
+        if (isValid(str)) {
+            System.out.println("The parentheses are valid.");
+        } else {
+            System.out.println("The parentheses are not valid.");
+        }
+        sc.close();
+    }
+}
