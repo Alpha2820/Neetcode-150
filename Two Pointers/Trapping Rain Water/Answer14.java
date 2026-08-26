59b9fdc1d8fa6bb2a4c72220eae71f86b0f3d4d6
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-08-26 19:21:15 +0530

    Time and Space Compexity have been added

--- Two Pointers/Trapping Rain Water/Answer14.java
+++ Two Pointers/Trapping Rain Water/Answer14.java
@@ -36,3 +36,13 @@ public class Answer14 {
     }
 
 }
+
+// Time Complexity: O(n) - The while loop runs until the left pointer is less
+// than the right pointer, which means it will run at most n times, where n is
+// the number of elements in the array. Therefore, the overall time complexity
+// is O(n).
+
+// Space Complexity: O(1) - The space used by the algorithm is constant, as we
+// are not using any additional data structures that grow with the input size.
+// The output variable does not count towards space complexity as it is required
+// to store the result.
